4b5094473e6f0b9a5f6d2dd0b2c3f40b44effdff
KIM-DO-HEE <kjkj014209@gmail.com>
2023-01-19 21:26:05 +0900

    Member, Address, Appointment, Post 연관관계 매핑 수정

--- src/main/java/io/k2c1/hereyougo/domain/Address.java
+++ src/main/java/io/k2c1/hereyougo/domain/Address.java
@@ -1,5 +1,8 @@
 package io.k2c1.hereyougo.domain;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class Address
 {
     private Long id;
@@ -7,4 +10,7 @@ public class Address
     private Basic basic; // 기초자치단체: 의정부시, 관악구 등
     private String detail; // 상세주소 (지번, 도로명)
     private String postalCode; // 우편번호
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
 }

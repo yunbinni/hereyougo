960cc2881bd6e1106fbf89dedba65e81893a3a69
yunbinni <yunbin9049@gmail.com>
2023-02-26 18:54:29 +0900

    Test Commit

--- src/main/java/io/k2c1/hereyougo/config/WebConfig.java
+++ src/main/java/io/k2c1/hereyougo/config/WebConfig.java
@@ -2,14 +2,16 @@ package io.k2c1.hereyougo.config;
 
 import io.k2c1.hereyougo.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig
+public class WebConfig implements WebMvcConfigurer
 {
     @Bean
     public FilterRegistrationBean loginCheckFilter()

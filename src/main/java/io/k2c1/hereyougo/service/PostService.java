79a831320ea80650797fd8cb4597d0f9e18d4008
KIM-DO-HEE <devkdhee@gmail.com>
2023-02-27 00:20:47 +0900

    feat : 글 상세 정보 조회
    - 글 상세정보 조회

--- src/main/java/io/k2c1/hereyougo/service/PostService.java
+++ src/main/java/io/k2c1/hereyougo/service/PostService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Transactional
 @Service
 public class PostService {
@@ -19,5 +21,9 @@ public class PostService {
     public void deleteByWriter(Long memberId){
         Member member = memberRepository.findById(memberId).get();
         postRepository.deleteByWriter(member);
+    }
+
+    public Optional<Post> getPost(Long postId){
+        return postRepository.findById(postId);
     }
 }

b2458de6fe4b1e4222b12a67ead6d4663d7e90ee
KIM-DO-HEE <devkdhee@gmail.com>
2023-02-26 21:47:56 +0900

    fix : 이미지 불러오지 못하는 문제 해결
    - addResourceHandlers를 이용하여 디렉토리와 url 매핑

--- src/main/java/io/k2c1/hereyougo/config/WebConfig.java
+++ src/main/java/io/k2c1/hereyougo/config/WebConfig.java
@@ -4,11 +4,14 @@ import io.k2c1.hereyougo.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig
+public class WebConfig implements WebMvcConfigurer
 {
     @Bean
     public FilterRegistrationBean loginCheckFilter()
@@ -18,5 +21,12 @@ public class WebConfig
         filterRegistrationBean.setOrder(2);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:images/")
+                .setCacheControl((CacheControl.noCache().cachePrivate()));
     }
 }

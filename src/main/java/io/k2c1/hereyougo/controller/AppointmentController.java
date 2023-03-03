9963ab916993129ec1649b180e7b7f11c974806c
KIM-DO-HEE <devkdhee@gmail.com>
2023-03-04 00:59:47 +0900

    feat : 약속 취소 로직 작성
    - 약속 취소를 할 경우, 예약 수량이 취소한 갯수만큼 증가한다
    - 예약 상태가 예약중->예약취소로 변겨오딘다
    
    Resolves: #43

--- src/main/java/io/k2c1/hereyougo/controller/AppointmentController.java
+++ src/main/java/io/k2c1/hereyougo/controller/AppointmentController.java
@@ -8,9 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -44,5 +42,11 @@ public class AppointmentController {
 
         model.addAttribute("message", message);
         return message;
+    }
+
+    @ResponseBody
+    @PutMapping("/{id}/status/cancel")
+    public void cancelAppointmentStatus(@PathVariable("id") Long appointmentId){
+        appointmentService.cancelAppointment(appointmentId);
     }
 }

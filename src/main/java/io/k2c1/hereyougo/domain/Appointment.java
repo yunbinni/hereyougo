9f5934d9f158575b1b3490d2a7a8ab7c6f9726f1
KIM-DO-HEE <devkdhee@gmail.com>
2023-03-03 17:46:59 +0900

    refactor : 채팅방 필드 추가
    - 약속 목록에서 거래명 클릭 시, 채팅방으로 이동할 수 있게 채팅방 필드 추가

--- src/main/java/io/k2c1/hereyougo/domain/Appointment.java
+++ src/main/java/io/k2c1/hereyougo/domain/Appointment.java
@@ -27,8 +27,18 @@ public class Appointment
     @JoinColumn(name = "wanted_id")
     private Member wanted;
 
-    @OneToOne
+//    @OneToOne
+    @ManyToOne
+    @JoinColumn(name = "post_id")
     private Post post;
+
+    /**
+     * 약속 목록에서 항목을 클릭할 경우
+     * 채팅방으로 이동되게 하기위해 chatroom_id를 선언
+     */
+    @ManyToOne
+    @JoinColumn(name = "chatroom_id")
+    private ChatRoom chatRoom;
 
     private int appointmentQuantity; // 예약 수량
 
